74e6ab0f821d7a7705da4728176e37236de68457
Abhay <abhay.johri0@gmail.com>
2023-10-09 01:31:06 +0530

    Transition effects added into help page

---
+++ Scripts/help.js
@@ -0,0 +1,36 @@
+let dropBtns = document.querySelectorAll('.drops');
+let sections = document.querySelectorAll('.cont');
+
+function resetAll(){
+    for(let sectn of sections){
+        var pTag = sectn.querySelector('p');
+        var spanTag = sectn.querySelector('span');
+
+        sectn.style.backgroundColor = 'var(--box-color)';
+        sectn.style.border = 'none';  
+
+        pTag.style.color = 'var(--icon-color-before)';
+        pTag.style.fontWeight = '650';
+
+        spanTag.style.transform = 'rotate(0deg)';
+        spanTag.style.color = 'var(--icon-color-before)';
+    }
+}
+
+for(let sectn of sections){
+    sectn.addEventListener('click', event =>{
+        resetAll();  
+        var target = event.target;
+        var pTag = event.target.querySelector('p');
+        var spanTag = event.target.querySelector('span');
+
+        target.style.backgroundColor = 'inherit';
+        target.style.border = '1px solid #0095ff';  
+
+        pTag.style.color = 'var(--icon-color-after)';
+        pTag.style.fontWeight = '500';
+        
+        spanTag.style.transform = 'rotate(-90deg)';
+        spanTag.style.color = 'var(--icon-color-after)';
+    });
+}
