1149476352d8e59c4d7b98ad5c61ba66a5a7654c
abhayjohri23 <abhay.johri0@gmail.com>
2023-10-20 04:10:34 +0530

    Added APIs to populate dataPane and aircondition Pane

--- Scripts/searchbar.js
+++ Scripts/searchbar.js
@@ -1,23 +1,67 @@
 let units = 'auto';
-
+const dbCoords = {
+    'Sunny' : [0,9],  
+    'Mostly sunny' : [0,9], 
+    'Partly sunny' : [25,91], 
+    'Mostly cloudy' : [25,91], 
+    'Cloudy' : [102,47], 
+    'Overcast' : [102,47], 
+    'Overcast with low clouds' : [102,49], 
+    'Fog': [25,49],
+    'Light rain' : [0,91],
+    'Rain': [74,9],
+    'Hail':[74,49],
+    'Possible rain' : [0,91],
+    'Rain shower' : [74,9],
+    'Thunderstorm' : [74,49],
+    'Local thunderstorms' : [0,91],
+    'Light snow' : [50,9], 
+    'Snow': [74,9],
+    'Possible snow' : [50,91], 
+    'Snow shower' : [102,9], 
+    'Rain and snow' : [102,9], 
+    'Possible rain and snow' : [50,91], 
+    'Rain and snow' : [102,0], 
+    'Freezing rain' : [102,9], 
+    'Possible freezing rain' : [50,9], 
+    'Clear (night)' : [50,49], 
+    'Clear' : [50,49], 
+    'Mostly clear (night)' : [50,49], 
+    'Partly clear (night)' : [25,9], 
+    'Mostly cloudy (night)' : [25,9], 
+    'Cloudy (night)' : [25,9], 
+    'Overcast with low clouds (night)' : [102,0], 
+    'Rain shower (night)' : [102,0], 
+    'Local thunderstorms (night)' : [0,49], 
+    'Snow shower (night)' : [0,49], 
+    'Rain and snow (night)' : [0,49], 
+    'Possible freezing rain (night)' : [102,0]   
+};
 function searchWeather(whichPage){
     const inputFieldText = document.querySelector('input.box').value;
     const objects = getPlaceDetails(inputFieldText);
-    const {name,country,lat,lon} = objects.then(data => {
-        let firstResult = data[0];
-        return firstResult;
-    });
 
     if(whichPage === 'Dashboard'){
-        let currentForecast = getCurrentForecast(lat,lon,units);
+        objects.then(data => {
+            return data[0];
+        })
+        .then(placeData => {
+            return [placeData.name, placeData.country, getCurrentForecast(placeData.lat,placeData.lon,units)];
+        })
+        .then(data => {
+            return fillDataPane(data[2],data[0],data[1]);
+        });
+
+
         let hourlyForecast = getHourlyForecast(lat,lon,units);
         let dailyForecast = getDailyForecast(lat,lon,units);
+        
         
 
     }
     else if(whichPage === 'Cities'){
         let cityArr = getNearbyPlaces(lat,lon);
-
+        let currentForecast;
     }
     else{
         return ;
@@ -124,4 +168,43 @@ function getNearbyPlaces(lat,lon){
     } catch (error) {
         alert(error);
     }
+}
+
+function fillDataPane(currentForecast,name,country){
+    currentForecast.then(data => {
+        let mainInfo = data.current;
+        let dataPane = document.querySelector('#dataPane');
+        let airConditionBox = document.querySelector('.airConditions');
+    
+        //Main Data Pane population 
+        dataPane.querySelector('#cityName').innerHTML = name;
+        console.log(dataPane.querySelector('#countryName'));
+        dataPane.querySelector('#countryName').innerHTML = ` , ${country}`;
+
+        dataPane.querySelector('#chanceOfRain').innerHTML = `Chance of rain: ${(100 - mainInfo.cloud_cover)}%`;
+        dataPane.querySelector('#avgTemp').innerHTML = `${mainInfo.temperature.toFixed(1).toString()}°`;
+        
+        let iconName = mainInfo.summary.toString();
+        let points = dbCoords[iconName];
+
+        console.log("Main Data consoles");
+        console.log(iconName);
+        console.log(points);
+
+        document.querySelector('.mainImageIcon').style.backgroundPosition = `${points[0]}% ${points[1]}%`;
+
+        //Air conditions Population
+        let airCondsOutput = airConditionBox.querySelectorAll('.airCondValue');
+        let airCondsInput = [mainInfo.feels_like,mainInfo.wind.speed,mainInfo.humidity,mainInfo.visibility,mainInfo.dew_point,mainInfo.uv_index,mainInfo.wind_chill,mainInfo.cloud_cover];
+        
+        console.log("Airconditions");
+        console.log(airCondsInput);
+
+        for(let i=0;i<airCondsInput.length;++i){
+            airCondsOutput[i].innerHTML = airCondsInput[i].toFixed(1).toString();
+        }
+    });
+
+
+    return ;
 }
